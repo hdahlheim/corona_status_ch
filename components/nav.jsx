7ac1c39423ca756b388abc063e887b271af9511f
hactar <hactar@cyberkraft.ch>
2020-03-15 13:54:17 +0100

    Fix nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function Nav() {
   return (
-    <nav className="max-w-md mx-auto">
-      <ul className="flex items-center justify-center gap-3 py-8">
+    <nav className="max-w-sm mx-auto">
+      <ul className="flex items-center justify-around py-8">
         <li>
           <Link href="/">
             <a className="text-blue-500 no-underline">Status</a>
